77d5cb51d0884944c103a287e3d4552e7fa83fc1
StupidRepo <flavouredtexts@icloud.com>
2021-09-04 16:17:16 +0100

    Added a test.js file for testing, added random function.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function Pi() {
-    return Math.PI
-}
+module.exports.Pi = Math.PI;
 
-module.exports.Pi = Pi;
+module.exports.Random = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+const { Pi, Random } = require("ssmath")
+// const { Pi, Random } = require("./main.js")
+
+console.log(Pi)
+console.log(Random(1, 3))
